0e8fae56a65f4af1a4ad712465e294d29ca04360
NS-Sheam <123sheamfeni@gmail.com>
2023-12-01 18:21:18 +0600

    13.9

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TStudent } from '../student/student.interface';
@@ -5,6 +6,8 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
 
 const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
   const userData: Partial<TUser> = {};
@@ -19,19 +22,34 @@ const createStudentIntoDB = async (password: string, payLoad: TStudent) => {
     payLoad.admissionSemester,
   );
 
-  userData.id = await generateStudentId(admissionSemester!);
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    userData.id = await generateStudentId(admissionSemester!);
 
-  // create a user
-  const newUser = await User.create(userData);
+    // create a user (Transaction-1)
+    const newUser = await User.create([userData], { session });
 
-  if (Object.keys(newUser).length) {
-    payLoad.id = newUser.id;
-    payLoad.user = newUser._id;
+    if (newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+    payLoad.id = newUser[0].id;
+    payLoad.user = newUser[0]._id;
+
+    // create a student (Transaction-2)
+    const newStudent = await Student.create([payLoad], { session });
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
   }
-
-  const newStudent = await Student.create(payLoad);
-
-  return newStudent;
 };
 
 export const UserServices = {
