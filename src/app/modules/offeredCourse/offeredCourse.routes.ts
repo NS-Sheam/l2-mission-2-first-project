fd69caf51f945d0ab125e0e0a8bff20c7c167b42
NS-Sheam <123sheamfeni@gmail.com>
2024-02-04 14:02:33 +0600

    Add authentication middleware to offeredCourse routes

--- src/app/modules/offeredCourse/offeredCourse.routes.ts
+++ src/app/modules/offeredCourse/offeredCourse.routes.ts
@@ -2,22 +2,43 @@ import { Router } from 'express';
 import { OfferedCourseController } from './offeredCourse.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { OfferedCourseValidations } from './offeredCourse.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.const';
 
 const router = Router();
 
-router.get('/', OfferedCourseController.getAllOfferedCourses);
-router.get('/:id', OfferedCourseController.getSingleOfferedCourse);
+router.get(
+  '/',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  OfferedCourseController.getAllOfferedCourses,
+);
+router.get(
+  '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseController.getSingleOfferedCourse,
+);
 
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
   OfferedCourseController.createOfferedCourse,
 );
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
   OfferedCourseController.updateOfferedCourse,
 );
-router.delete('/:id', OfferedCourseController.deleteOfferedCourse);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  OfferedCourseController.deleteOfferedCourse,
+);
 
 export const offeredCourseRoutes = router;
