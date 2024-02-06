506d4fbf0ec0f9af50bd7ed5ed3d0e04f3b3f50e
NS-Sheam <123sheamfeni@gmail.com>
2024-02-06 12:01:14 +0600

    Update error status code in academicSemester.model.ts

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -46,7 +46,7 @@ academicSemesterSchema.pre('save', async function (next) {
     year: this.year,
   });
   if (isSemesterExists) {
-    throw new AppError(httpStatus.NOT_FOUND, 'Semester already exists');
+    throw new AppError(httpStatus.BAD_REQUEST, 'Semester already exists');
   }
   next();
 });
